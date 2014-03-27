b24289bd75f741a4ef9416af0efb59eb9e54b764
Lucas Aquino <luxcas@gmail.com>
2014-03-27 10:08:25 -0300

    aborta missão

--- lx/tools/config.py
+++ lx/tools/config.py
@@ -6,7 +6,3 @@ PROJECTNAME = 'lx.tools'
 ADD_PERMISSIONS = {
     # -*- extra stuff goes here -*-
 }
-
-PRODUCTS = [('lx.tools', 0 ,1 ,0 , 'lx.tools:default', 1),
-            ('lx.tools', 0 ,1 ,0 , 'lx.tools:uninstall', 1),
-           ]
