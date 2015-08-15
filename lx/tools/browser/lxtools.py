aefdae63f313ff1297840ef404a6ccae0e9156f9
Lucas Aquino <luxcas@gmail.com>
2015-08-15 11:01:32 -0300

    retira casti

--- lx/tools/browser/lxtools.py
+++ lx/tools/browser/lxtools.py
@@ -11,8 +11,7 @@ import logging
 from zope.site.hooks import getSite
 from zope.component import queryUtility
 from plone.i18n.normalizer.interfaces import IIDNormalizer
-from xml.etree.ElementTree import iterparse
-from je.casti.interfaces.interfaces import ICatalogoServicoPrefsForm
+from xml.etree.ElementTree import iterparseICatalogoServicoPrefsForm
 from zope.component import getUtility
 from plone.registry.interfaces import IRegistry
 #Libs python
@@ -42,11 +41,7 @@ class LxToolsView(BrowserView):
             export_version = self.request.get('exportVersionXML', None)
             if self.validateParseXML(file_xml, folder_conteudo, export_version):
                 return self.parseXML(file_xml, folder_conteudo, export_version)
-        if 'form.action.tipoServico' in self.request.form:
-            tipo_servico = self.request.get('tipoServico', None)
-            folder_catalogo = self.request.get('folderCatalogo', None)
-            if self.validateTipoServico(tipo_servico, folder_catalogo):
-                return self.createAtividade(tipo_servico, folder_catalogo)
+
 
     def validateReindexIndex(self, tipo_conteudo, index_conteudo):
         """Validação do Reindex.
@@ -196,99 +191,4 @@ class LxToolsView(BrowserView):
             return False
         else:
             return True
-
-    def createAtividade(self, tipo_servico, folder_catalogo):
-        """
-        """
-        log = logging.getLogger('createAtividade:')
-        ctool = getToolByName(self.context, 'portal_catalog')
-        #folder_conteudo = 'catalogo'
-        site = getSite()
-        pw = getToolByName(site, 'portal_workflow')
-        publish = pw.doActionFor
-        idnormalizer = queryUtility(IIDNormalizer)
-        #id_folder = idnormalizer.normalize(folder_conteudo)
-        path = folder_catalogo.split('/')
-        if path[-1] == '':
-            id_folder = path[:-1][-1]
-            path = '/'.join(path[:-2])
-        else:
-            id_folder = path[-1]
-            path = '/'.join(path[:-1])
-        folder = ctool(portal_type='Folder', id=id_folder, path={"query": path, "depth": 1})[0]
-        folderCASTI = folder.getObject()
-        #if not hasattr(site, id_folder):
-        #    site.invokeFactory('Folder', id=id_folder, title=folder_conteudo)
-        #folderCASTI = getattr(site, id_folder)
-
-        registry = getUtility(IRegistry)
-        settings = registry.forInterface(ICatalogoServicoPrefsForm)
-        url = str(settings.url_webservice)
-        client = Client(url)
-        filtro = client.factory.create('wsFiltros')
-
-        if tipo_servico == '1':
-            filtro.tipoServico.id = 1
-            filtro.tipoServico.sigla = "INFR"
-            nome_servico = _(u'Infraestrutura')
-
-        if tipo_servico == '2':
-            filtro.tipoServico.id = 2
-            filtro.tipoServico.sigla = "CRTL"
-            nome_servico = _(u'Apoio ao Controle')
-
-        if tipo_servico == '3':
-            filtro.tipoServico.id = 3
-            filtro.tipoServico.sigla = "SUST"
-            nome_servico = _(u'Sustentação')
-
-        if tipo_servico == '4':
-            filtro.tipoServico.id = 4
-            filtro.tipoServico.sigla = "PLAN"
-            nome_servico = _(u'Apoio ao Planejamento')
-
-        search = client.service.consultarAtividades(filtro)
-        result = []
-
-        id_processo = str(filtro.tipoServico.id) + '-' + idnormalizer.normalize(nome_servico)
-        if not hasattr(folderCASTI, id_processo):
-            folderCASTI.invokeFactory('Folder', id=id_processo, title=nome_servico)
-            folderProcesso = getattr(folderCASTI, id_processo)
-            folderProcesso.setLayout('lista-atividade')
-            publish(folderProcesso, 'publish')
-        else:
-            folderProcesso = getattr(folderCASTI, id_processo)
-
-        for item in search.item:
-            title_subprocesso = item.codigo + ' - ' + item.descricao
-            id_subprocesso = idnormalizer.normalize(title_subprocesso)
-
-            if not hasattr(folderProcesso, id_subprocesso):
-                folderProcesso.invokeFactory('Folder', id=id_subprocesso, title=title_subprocesso)
-                folderSubProcesso = getattr(folderProcesso, id_subprocesso)
-                folderSubProcesso.setLayout('lista-atividade')
-                publish(folderSubProcesso, 'publish')
-
-            for atividade in item.listAtividade:
-                item = {'id': tipo_servico + '-' + item.codigo + '-' + atividade.codigo,
-                        'codigo': atividade.codigo,
-                        'descricao': atividade.descricao,
-                        'subprocesso': id_subprocesso}
-                result.append(item)
-
-        for i in result:
-            subprocesso = i['subprocesso']
-            folderSubProcesso = getattr(folderProcesso, subprocesso)
-            id = i['id']
-            if not hasattr(folderSubProcesso, id):
-                title_atividade = i['codigo'] + ' - ' + i['descricao']
-                folderSubProcesso.invokeFactory('Atividade',
-                                                id=id,
-                                                title=title_atividade,
-                                                codigo_atividade=i['codigo'],
-                                                tipo_servico=tipo_servico,
-                                                allowDiscussion=True)
-                at = getattr(folderSubProcesso, id)
-                publish(at, 'publish')
-                log.info(id)
 
